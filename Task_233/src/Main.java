c3427bce63e8efbe6533475e4d020e7d1665698c
TBatsenkova <batsenkova2020@gmail.com>
2022-03-01 00:07:54 +0300

    Add Task_233

---
+++ Task_233/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    static int a;
+    static int b = 3;
+    static int c;
+
+    public static void main(String[] args) throws ArithmeticException {
+        method1();
+        System.out.println();
+    }
+    public static void method1() throws ArithmeticException {
+        int a = (int) (Math.random() * 0);
+        c = b / a;
+    }
+}
